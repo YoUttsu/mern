6d9b44c6b5b3959f8591712845ab77ee271dbb8d
youttsu <youttsu@gmail.com>
2023-01-01 14:09:15 +0530

    test

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,3 @@
 //you can use npm run "script name"("" dont add this) to run any script which are specified in package.json
 //to install something as devdependencies you have to use npm i -D packagename (usually you install nodemon as devdependencies ✌️)
-console.log("hwllo");
+console.log("hii");
